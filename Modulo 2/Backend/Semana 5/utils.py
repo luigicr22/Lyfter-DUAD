3d1b13ac570051f23253b7ca3a9f39f2212a5423
Luis Hidalgo <luis.hidalgomora@outlook.com>
2026-01-19 12:57:33 -0600

    Cambios solicitados

--- Modulo 2/Backend/Semana 5/utils.py
+++ Modulo 2/Backend/Semana 5/utils.py
@@ -14,20 +14,25 @@ def write_json(tasks):
     return True
 
 def check_fields_task(task):
-    requeired_fields = ["id", "title", "description", "status"]
-    for field in requeired_fields:
+    required_fields = ["id", "title", "description", "status"]
+    required_status = ["Por Hacer", "En Progreso", "Completada"]
+    for field in required_fields:
         if task.get(field) is None:
             raise KeyError(field)
+    if task.get('status') not in required_status:
+        raise ValueError("El campo status tiene estado invalido")
+    if not type(task.get('id')) is int:
+        raise ValueError("El id debe ser un número entero mayor a cero")
     return True
 
 def check_not_existed (task_to_check,tasks):
     for task in tasks:
         if task_to_check.get('id') == task.get('id'):
-            raise ValueError(task.get('id'))
+            raise ValueError(f"El id {task.get('id')} ya existe")
     return True
 
 def check_existed (task_to_check,tasks):
     for index in range(len(tasks)):
         if task_to_check.get('id') == tasks[index].get('id'):
             return index
-    raise ValueError(task_to_check.get('id'))
+    raise ValueError(f"El id {task_to_check.get('id')} no existe")
